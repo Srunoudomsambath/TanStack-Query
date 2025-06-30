9953015a52175d4b67015427a9dbb3b15df24542
srunoudomsambath <toyroith@gmail.com>
2025-06-30 11:01:30 +0700

    Updating somthing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div 
-    >
+    className="grid grid-cols-2">
+      <div>
      <h1 className="text-[150px]">Srun Oudomsambath * Roith</h1>
       <h2 className="text-3xl font-ubuntu">Lorem ipsum dolor sit amet. </h2>
       <Image 
@@ -13,6 +14,7 @@ export default function Home() {
      height={100}
      className="bg-amber-800 border-8 border-amber-800 rounded-2xl"
      />
+     </div>
      <div className="bg-black ">
       <UserCard/>
      </div>
